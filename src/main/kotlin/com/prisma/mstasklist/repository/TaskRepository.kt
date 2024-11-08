a7dbe6a1e070a9ba8554a16d45b86039155c97be
gpavichevic <gpavichevich@serv.externos.prismamp.com>
2024-11-08 17:11:36 -0300

    Update the project into version java

--- src/main/kotlin/com/prisma/mstasklist/repository/TaskRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.prisma.mstasklist.repository
-
-import com.prisma.mstasklist.model.Task
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TaskRepository : JpaRepository<Task, Long>
